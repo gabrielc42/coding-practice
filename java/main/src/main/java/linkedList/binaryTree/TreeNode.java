5445854c8a9da4e79892f1db1b20710999322a72
gabrielc42 <clark.gabe97@gmail.com>
2023-01-23 19:11:10 -0600

    binaryTree java

---
+++ java/main/src/main/java/linkedList/binaryTree/TreeNode.java
@@ -0,0 +1,14 @@
+package linkedList.binaryTree;
+
+// Use this class to create binary trees.
+public class TreeNode {
+    int value;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int value, TreeNode left, TreeNode right) {
+        this.value = value;
+        this.left = left;
+        this.right = right;
+    }
+}
