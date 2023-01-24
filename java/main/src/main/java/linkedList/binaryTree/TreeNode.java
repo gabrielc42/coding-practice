51d3963503f042512165b25970336fb5f163657d
gabrielc42 <clark.gabe97@gmail.com>
2023-01-23 20:00:11 -0600

    lca java nullPointerException

--- java/main/src/main/java/linkedList/binaryTree/TreeNode.java
+++ java/main/src/main/java/linkedList/binaryTree/TreeNode.java
@@ -1,5 +1,7 @@
 package linkedList.binaryTree;
 
+import java.util.Objects;
+
 // Use this class to create binary trees.
 public class TreeNode {
     int value;
@@ -10,5 +12,21 @@ public class TreeNode {
         this.value = value;
         this.left = left;
         this.right = right;
+    }
+
+//    @Override
+//    public String toString() {
+//        return "TreeNode{" +
+//                "value=" + value +
+//                ", left=" + left +
+//                ", right=" + right +
+//                '}';
+//    }
+
+    // The string representation of this node.
+    // Will be used for testing.
+    @Override
+    public String toString() {
+        return String.valueOf(value);
     }
 }
