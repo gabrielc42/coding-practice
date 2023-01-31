eefbf2e25cfa4ee9184bb939225c5eae6e76042b
gabrielc42 <clark.gabe97@gmail.com>
2023-01-31 17:27:46 -0600

    maps, filter, reduce problem studies & JS-TS README.md with links approprios

---
+++ js-ts/README.md
@@ -0,0 +1,5 @@
+﻿# coding-practice [Javascript and Typescript]
+
+## [mapsFilterReduce study](https://www.crocoder.dev/blog/map-filter-reduce-exercises/)
+
+## [truthy](https://gist.github.com/esin87/ff63e69a02e1a61995d9dd968f969080)
--- js-ts/mapsFilterReduce/main.js
+++ js-ts/mapsFilterReduce/main.js
@@ -26,7 +26,7 @@ console.log(input);
 // median and mean
 
 // input = [12, 46, 32, 64];
-//  { mean: 38.5, median: 39 }
+// { mean: 38.5, median: 39 }
 
 input = [12, 46, 32, 64];
 input.sort((a, b) => a - b);
@@ -68,3 +68,193 @@ init = input
   .join("");
 
 console.log(init);
+
+
+// age difference from youngest and oldest
+
+input = [
+  {
+    name: "Sugar",
+    age: 13,
+  },
+  {
+    name: "Santos",
+    age: 56,
+  },
+  {
+    name: "Rachel",
+    age: 45,
+  },
+  {
+    name: "Nathan",
+    age: 67,
+  },
+  {
+    name: "Jennifer",
+    age: 65,
+  },
+];
+
+// [13, 67, 54];
+
+ages = input.map((person) => person.age); 
+difference = [Math.min(...ages), Math.max(...ages), Math.max(...ages) - Math.min(...ages)];
+
+console.log(difference);
+
+
+// numeronyms
+
+input = "Every developer likes to mix kubernetes and javascript";
+// "E3y d7r l3s to mix k8s and j8t";
+
+createNumeronym = (word) => 
+  word[0] + (word.length - 2) + word[word.length - 1];
+
+numeronym = input
+  .split(" ")
+  .map((word) => (word.length >= 4 ? createNumeronym(word) : word))
+  .join(" ");
+
+console.log(numeronym);
+
+
+
+// n! w/ map and reduce
+
+input = 6;
+// 720
+
+array = new Array(input)
+  .fill(null)
+  .map((currentValue, index) => index + 1)
+  .reduce((accumulator, currentValue) => accumulator * currentValue);
+
+console.log(array);
+
+
+// count elements in array of arrays
+
+input = [
+  ["a", "b", "c"],
+  ["c", "d", "f"],
+  ["d", "f", "g"],
+];
+
+// {
+//   a: 1,
+//   b: 1,
+//   c: 2,
+//   d: 2,
+//   f: 2,
+//   g: 1,
+// }
+
+count = input
+  .flat()
+  .reduce((accumulator, currentValue) => {
+    if (accumulator[currentValue]) {
+      accumulator[currentValue] += 1;
+    } else {
+      accumulator[currentValue] = 1;
+    }
+    return accumulator;
+  }, {});
+
+console.log(count);
+
+
+// high performing students
+
+students = [
+  { name: "Alice", scores: [90, 85, 92] },
+  { name: "Bob", scores: [75, 80, 85] },
+  { name: "Charlie", scores: [90, 95, 85] },
+  { name: "Jack", scores: [100, 100, 100] }
+];
+
+// [ 
+//   { name: 'Jack', average: 100 }
+// ]
+
+studentAverages = students.map(student => {
+  sum = student.scores.reduce((acc, score) => acc + score);
+  return { name: student.name, average: sum / student.scores.length };
+});
+
+highPerformers = studentAverages.filter(student => student.average > 90);
+
+console.log(highPerformers);
+
+
+// high priced product categories
+
+products = [
+  { name: "Product 1", price: 20, category: "Electronics" },
+  { name: "Product 2", price: 30, category: "Clothes" },
+  { name: "Product 3", price: 40, category: "Electronics" },
+  { name: "Product 4", price: 50, category: "Clothes" },
+  { name: "Product 5", price: 60, category: "Clothes" },
+  { name: "Product 6", price: 70, category: "Electronics" },
+  { name: "Product 7", price: 80, category: "Clothes" },
+  { name: "Product 8", price: 90, category: "Electronics" },
+];
+
+// [ 
+//   { category: 'Clothes', average: 55 },
+//   { category: 'Electronics', average: 55 }
+// ]
+
+productsByCategory = products.reduce((acc, product) => {
+  category = product.category;
+  if (!acc[category]) {
+    acc[category] = [];
+  }
+  acc[category].push(product);
+  return acc;
+}, {});
+
+avgPriceByCategory = Object.keys(productsByCategory).map(category => {
+  sum = productsByCategory[category].reduce((acc, product) => acc + product.price, 0);
+  return { category: category, average: sum / productsByCategory[category].length };
+})
+
+highPricedCategories = avgPriceByCategory.filter(category => category.average > 50);
+
+console.log(highPricedCategories);
+
+
+// hr vs it department
+
+employees = [
+  { name: "John", salary: 50000, department: "IT" },
+  { name: "Jane", salary: 60000, department: "HR" },
+  { name: "Bob", salary: 55000, department: "IT" },
+  { name: "Sophie", salary: 75000, department: "HR" },
+  { name: "Mike", salary: 65000, department: "IT" },
+  { name: "Emily", salary: 80000, department: "HR" },
+  { name: "David", salary: 70000, department: "IT" },
+];
+
+// [
+//   { department: 'HR', average: 71666 }
+// ]
+
+employeesByDepartment = employees.reduce((acc, employee) => {
+  department = employee.department;
+  if (!acc[department]) {
+    acc[department] = [];
+  }
+  acc[department].push(employee);
+  return acc;
+}, {});
+
+avgSalaryByDepartment = Object.keys(employeesByDepartment)
+  .map(department => {
+    sum = employeesByDepartment[department].reduce((acc, employee) => acc + employee.salary, 0);
+    return { department: department, average: sum / employeesByDepartment[department].length };
+  })
+
+highPaidDepartments = avgSalaryByDepartment.filter(department => department.average > 65000);
+
+console.log(highPaidDepartments);
