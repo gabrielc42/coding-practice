515aa9852835eb9f4230e8eaa68ca85d3e0847a1
gabrielc42 <clark.gabe97@gmail.com>
2023-02-17 20:27:59 -0600

    errors with this example?

--- rust/unsafe_pointers/src/main.rs
+++ rust/unsafe_pointers/src/main.rs
@@ -1,3 +1,5 @@
+use std::rc::Rc;
+
 fn main() {
     println!("Hello, world!");
 
@@ -19,4 +21,20 @@ fn main() {
     vec.clear();
 
     println!("{}", unsafe { *vec_ptr }); // this causes a segmentation fault
+
+    // this example seems deprecated?
+    // let mut rc_vec = Rc::new(Vec::new());
+    // rc_vec.push(1);
+    // rc_vec.push(2);
+
+    // let vec_ptr = &vec[0];
+
+    // rc_vec.clear();
+
+    // println!("{}", unsafe { *vec_ptr }); // will not cause segmentation fault
+
+    // let life_vec = vec![1, 2, 3];
+    // let vec_ptr: &'static i32 = &life_vec[0];
+
+    // println!("{}", unsafe { *vec_ptr }); // safe
 }
