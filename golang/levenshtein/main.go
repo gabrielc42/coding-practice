39339f7a08f3d65550c3962dacc647b9cfab32f9
gabrielc42 <clark.gabe97@gmail.com>
2023-04-20 11:06:14 -0500

    golang levenshtein practice init

---
+++ golang/levenshtein/main.go
@@ -0,0 +1,8 @@
+package main
+
+func main() {
+	// Levenshtein distance: a string metric for measuring difference between two sequences
+	// calculated by preparing a matrix of size (M+1)x(N+1)
+	// —where M and N are the lengths of the 2 words—and looping through said matrix using 2 for loops,
+	// performing some calculations within each iteration.
+}
